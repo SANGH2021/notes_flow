7b5bbe9cc5e141530657dc6cde17470169051f5f
SANGH2021 <ssanghratna@gmail.com>
2025-11-16 00:10:26 +0530

    notes update page

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: AddReadDeleteUpdatePage(),
+      home: SplashScreen(),
     );
   }
 }
